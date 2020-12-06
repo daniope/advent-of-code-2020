92b3b336a81e724dc84f1c5026cc6f0c585cf1da
daniope <daniela.iope@hotmail.com>
2020-12-06 17:55:04 -0300

    feat: restricts getPairs to values that sum 2020

--- day01/Main.hs
+++ day01/Main.hs
@@ -2,10 +2,16 @@ module Main where
 
 import Data.List
 
+mul :: (Integer, Integer) -> Integer
+mul (x, y) = x * y
+
 getPairs :: [Integer] -> [(Integer, Integer)]
 getPairs [] = []
-getPairs l = [(x, y) | (x:ys) <- tails l, y <- ys]
+getPairs l = [(x, y) | (x:ys) <- tails l, y <- ys, x + y == 2020]
 
+solve :: [Integer] -> [Integer]
+solve l = map mul (getPairs l)
+
+main :: IO ()
 main = do
-    putStrLn "Pairs:"
-    print(getPairs [1, 2, 3, 4, 5])
+    print(solve [1721, 979, 366, 299, 675, 1456])
