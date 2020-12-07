6353e8ee6c5c1fa5491c943ca8603a2ca197da78
daniope <daniela.iope@hotmail.com>
2020-12-06 21:30:27 -0300

    feat: asks for file path as input

--- day01/Main.hs
+++ day01/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
 import Data.List
-import System.IO
+import System.Environment
 
 parseInts :: [String] -> [Int]
 parseInts xs = map read xs
 
 readNumbers :: FilePath -> IO [Int]
-readNumbers = (fmap parseInts . (fmap lines . readFile))
+readNumbers = fmap parseInts . (fmap lines . readFile)
 
 getPairs :: [Int] -> Int -> [(Int, Int)]
 getPairs xs year = [(x, y) | (x:ys) <- tails xs, y <- ys, x + y == year]
@@ -20,7 +20,8 @@ solve xs year = map mul (getPairs xs year)
 
 main :: IO ()
 main = do
-    numbers <- readNumbers "input.txt"
+    args <- getArgs
+    numbers <- readNumbers $ head args
     let result = solve numbers 2020
     putStr "Solution: "
     print result
