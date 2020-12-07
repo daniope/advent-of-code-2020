dc97f3a2331c473cec424fa45c92db8fa4714183
daniope <daniela.iope@hotmail.com>
2020-12-06 21:46:31 -0300

    feat: impls part 2

--- day01/Main.hs
+++ day01/Main.hs
@@ -9,19 +9,35 @@ parseInts xs = map read xs
 readNumbers :: FilePath -> IO [Int]
 readNumbers = fmap parseInts . (fmap lines . readFile)
 
-getPairs :: [Int] -> Int -> [(Int, Int)]
-getPairs xs year = [(x, y) | (x:ys) <- tails xs, y <- ys, x + y == year]
+getPairs :: [Int] -> Int -> [[Int]]
+getPairs xs year = [[x, y] | (x:ys) <- tails xs, y <- ys, x + y == year]
+
+getTrios :: [Int] -> Int -> [[Int]]
+getTrios xs year = [
+    [x, y, z]
+    | (x:ys) <- tails xs
+    , (y:zs) <- tails ys 
+    , z <- zs
+    , x + y + z == year
+    ]
 
 mul :: (Int, Int) -> Int
 mul (x, y) = x * y
 
-solve :: [Int] -> Int -> [Int]
-solve xs year = map mul (getPairs xs year)
+prod :: [Int] -> Int
+prod []     = 1
+prod (x:xs) = x * prod xs
+
+solve :: [Int] -> Int -> Int -> [Int]
+solve xs 2 year = map prod (getPairs xs year)
+solve xs 3 year = map prod (getTrios xs year)
+solve xs _ year = []
 
 main :: IO ()
 main = do
     args <- getArgs
     numbers <- readNumbers $ head args
-    let result = solve numbers 2020
-    putStr "Solution: "
-    print result
+    putStr "Part 1: "
+    print $ solve numbers 2 2020
+    putStr "Part 2: "
+    print $ solve numbers 3 2020
