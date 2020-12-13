c8a567cc2faf702aaf7c68e03f216c3560124e08
daniope <daniela.iope@hotmail.com>
2020-12-13 01:02:20 -0300

    feat: impls parser for policy

--- day02/Main.hs
+++ day02/Main.hs
@@ -1,25 +1,38 @@
 module Main where
 
 import Data.Char
+import Text.Parsec
 import Text.Parsec.Char
 import Text.Parsec.String
-import Text.ParserCombinators.Parsec
 import System.Environment
 
 data Policy = Policy {
       min :: Int
     , max :: Int
-    , letter :: Char
+    , character :: Char
     , password :: String
     } deriving (Eq, Show) 
 
-integer :: CharParser st Int
-integer = fmap read (many $ satisfy isDigit)
+integer :: Parser Int
+integer = fmap read $ many1 digit
+
+policy :: Parser Policy
+policy = do 
+    { min       <- integer
+    ; _         <- oneOf "-"
+    ; max       <- integer
+    ; _         <- space
+    ; character <- anyChar
+    ; _         <- oneOf ":"
+    ; _         <- space
+    ; password  <- many1 letter
+    ; return (Policy min max character password)
+    }
 
 main :: IO ()
 main = do 
     { args <- getArgs
-    ; result <- parseFromFile integer $ head args
+    ; result <- parseFromFile policy $ head args
     ; case result of
         Left err  -> print err
         Right xs  -> print xs
