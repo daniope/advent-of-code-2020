ca0c69db073bd164881dd95155f74ccf3b9489fc
daniope <daniela.iope@hotmail.com>
2020-12-12 23:19:41 -0300

    feat: impls data structure

--- day02/Main.hs
+++ day02/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
+import Data.Char
+import Text.Parsec.Char
+import Text.Parsec.String
+import Text.ParserCombinators.Parsec
+import System.Environment
+
+data Policy = Policy {
+      min :: Int
+    , max :: Int
+    , letter :: Char
+    , password :: String
+    } deriving (Eq, Show) 
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do 
+    { args <- getArgs
+    ; result <- parseFromFile anyChar $ head args
+    ; case result of
+        Left err  -> print err
+        Right xs  -> print xs
+    }
