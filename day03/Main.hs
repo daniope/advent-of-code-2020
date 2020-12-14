2f329d1368939998a578079e687affef89e6cb00
daniope <daniela.iope@hotmail.com>
2020-12-13 21:45:09 -0300

    feat: impls solution for part 2

--- day03/Main.hs
+++ day03/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.Tuple
 import System.Environment
 
 data Element =
@@ -12,26 +13,34 @@ parse_element el = case el of
     '.' -> Empty
     '#' -> Tree
 
-get_element :: [String] -> Int -> Int -> Element
-get_element s row col = do
-    { let el = (s !! row) !! col
+get_element :: [String] -> (Int,Int) -> Element
+get_element s coord = do
+    { let el = (s !! (fst coord)) !! (snd coord)
     ; parse_element el
     }
 
+get_coords :: [String] -> Int -> Int -> [(Int, Int)]
+get_coords s right down = map swap $ zip [0,right..] [0,down..length s - 1]
+
 get_elements :: [String] -> Int -> Int -> [Element]
 get_elements s right down = 
-    [ get_element s row (rem col $ length $ s !! 0)
-    | (col, row) <- zip [0,right..] [0,down..length s - 1]
+    [ get_element s (row, (rem col $ length $ head s))
+    | (row, col) <- get_coords s right down
     ]
 
 get_trees :: [Element] -> [Element]
 get_trees els = filter (== Tree) els
 
-solve :: [String] -> Int -> Int -> Int
-solve ss down right = do
-    { let els = get_elements ss down right
+solve_1 :: [String] -> Int -> Int -> Int
+solve_1 ss right down = do
+    { let els = get_elements ss right down
     ; let trees = get_trees els
     ; length trees
+    }
+
+solve_2 :: [String] -> [(Int,Int)] -> Int
+solve_2 ss slopes = do
+    { product $ map (\x -> solve_1 ss (fst x) (snd x)) slopes
     }
 
 main :: IO ()
@@ -39,5 +48,7 @@ main = do
     { args <- getArgs
     ; content <- fmap lines $ readFile $ head args
     ; putStr "Part 1: "
-    ; print $ solve content 3 1 
+    ; print $ solve_1 content 3 1 
+    ; putStr "Part 2: "
+    ; print $ solve_2 content [(1,1), (3,1), (5,1), (7,1), (1,2)]
     }
