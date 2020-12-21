9e4ba1586b6832c03a3f1621b077d0aec68493a8
daniope <daniela.iope@hotmail.com>
2020-12-21 18:03:02 -0300

    feat: impls reading of input

--- day05/Main.hs
+++ day05/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import System.Environment
+
+readPasses :: FilePath -> IO [String]
+readPasses = fmap lines . readFile
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    { args <- getArgs
+    ; passes <- readPasses $ head args
+    ; print passes
+    }
