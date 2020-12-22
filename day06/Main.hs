dfd6a940608c08e667be4cdfaa78cb0af7cd94a9
daniope <daniela.iope@hotmail.com>
2020-12-22 00:23:23 -0300

    feat: impls part 2

--- day06/Main.hs
+++ day06/Main.hs
@@ -18,16 +18,22 @@ group = endBy1 form endOfLine
 groups :: Parser Groups
 groups = sepBy group newline
 
-answer :: Char -> Group -> Bool
-answer c []     = False
-answer c (f:fs) = elem c f || answer c fs 
+answer :: Char -> Group -> Int -> Bool
+answer c [] 1     = False
+answer c [] 2     = True
+answer c (f:fs) 1 = elem c f || answer c fs 1
+answer c (f:fs) 2 = elem c f && answer c fs 2
 
-sheet :: Group -> [Char]
-sheet g = [ x | x <- ['a'..'z'], answer x g ]
+sheet :: Group -> Int -> [Char]
+sheet g part = [ x | x <- ['a'..'z'], answer x g part]
 
 part1 :: Groups -> Int
 part1 []     = 0
-part1 (g:gs) = (length $ sheet g) + part1 gs 
+part1 (g:gs) = (length $ sheet g 1) + part1 gs 
+
+part2 :: Groups -> Int
+part2 []     = 0
+part2 (g:gs) = (length $ sheet g 2) + part2 gs 
 
 main :: IO ()
 main = do
@@ -35,5 +41,8 @@ main = do
     ; input <- parseFromFile groups $ head args
     ; case input of
         Left err  -> print err
-        Right gs  -> putStrLn $ "Part 1: " ++ show (part1 gs)
+        Right gs  -> do
+            { putStrLn $ "Part 1: " ++ show (part1 gs)
+            ; putStrLn $ "Part 2: " ++ show (part2 gs)
+            }
     }
