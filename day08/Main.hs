7fd89d0b2e0a9d0967185c531712f18c9589e07e
daniope <daniela.iope@hotmail.com>
2020-12-28 21:00:18 -0300

    feat: impls solution for part 2

--- day08/Main.hs
+++ day08/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List
 import Text.Parsec
 import Text.Parsec.String
 import System.Environment
@@ -15,7 +16,7 @@ type Command = (Operation, Argument)
 
 data Stop =
       InfiniteLoop
-    | End
+    | EOF
     | None
     deriving (Show)
 
@@ -67,17 +68,41 @@ accumulate _        n = n
 
 run :: Boot -> Boot
 run b
-    | curr >= size          = setStop b End
+    | curr >= size          = setStop b EOF
     | elem curr (history b) = setStop b InfiniteLoop
     | otherwise             = run $ Boot acc next hist (code b) None
     where size = length $ code b
           curr = step b
+          c    = (code b) !! curr
           next = jump (code b !! curr) curr
-          hist = curr : history b
+          hist = history b ++ [curr]
           acc  = accumulate (code b !! curr) (accumulator b)
 
 solve1 :: [Command] -> Boot
 solve1 cs = run (Boot 0 0 [] cs None)
+
+replace :: Int -> [Command] -> [Command]    
+replace i cs = take i cs ++ [(o, snd c)] ++ drop (i+1) cs
+    where c = cs !! i 
+          o = case (fst c) of
+              JMP -> NOP
+              NOP -> JMP
+              _   -> o
+
+getSteps :: [Command] -> [Int]
+getSteps cs = [ i | (c, i) <- zip cs [0..], fst c `elem` [JMP, NOP] ]
+
+solve2 :: [Command] -> [Int] -> Boot
+solve2 cs [] = run $ Boot 0 0 [] cs None
+solve2 cs t = core $ getSteps cs
+    where
+        core :: [Int] -> Boot
+        core (s:ss) = do
+            { let b = run $ Boot 0 0 [] (replace s cs) None
+            ; case (stop b) of
+                EOF -> b
+                _   -> core ss
+            }
 
 main :: IO ()
 main = do
@@ -87,5 +112,6 @@ main = do
         Left err -> print err
         Right cs -> do
             { putStrLn $ "Part 1: " ++ show (accumulator $ solve1 cs)
+            ; putStrLn $ "Part 2: " ++ show (accumulator $ solve2 cs $ getSteps cs)
             }
     }
