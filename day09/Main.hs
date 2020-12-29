087f4763180c7bbc3dc753f4f09ccb06e002bfbf
daniope <daniela.iope@hotmail.com>
2020-12-28 22:28:33 -0300

    refactor: part 1 returns Maybe

--- day09/Main.hs
+++ day09/Main.hs
@@ -10,23 +10,26 @@ parse = fmap rd . (fmap lines . readFile)
     where rd = map read
 
 valid :: [Int] -> Int -> Bool
-valid xs n = length pairs > 0
+valid xs n = not $ null pairs
     where pairs = [ [x, y] | (x:ys) <- tails xs, y <- ys, x + y == n]
 
 section :: [Int] -> Int -> Int -> [Int]
 section xs i n = (take n . drop i) xs
 
-solve1 :: [Int] -> Preamble -> [Int]
-solve1 xs p =
-    [ x
-    | (x, i) <- zip (drop p xs) [p..]
-    , not $ valid (section xs (i - p) p) x
-    ]
+solve1 :: [Int] -> Preamble -> Maybe Int
+solve1 xs p = case (length ys) of
+    0 -> Nothing
+    _ -> Just $ head ys
+    where ys =
+            [ x
+            | (x, i) <- zip (drop p xs) [p..]
+            , not $ valid (section xs (i - p) p) x
+            ]
 
 main :: IO ()
 main = do
     { args <- getArgs
     ; xs <- parse $ head args
     ; let preamble = read $ args !! 1
-    ; putStrLn $ show (head $ solve1 xs preamble)
+    ; putStrLn $ "Part 1: " ++ show (solve1 xs preamble)
     }
