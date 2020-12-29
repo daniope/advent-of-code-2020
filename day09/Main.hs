029a7e5751fa4a020e1eba0d1dfe5330da117206
daniope <daniela.iope@hotmail.com>
2020-12-28 23:41:37 -0300

    feat: impls solution for part 2

--- day09/Main.hs
+++ day09/Main.hs
@@ -16,20 +16,35 @@ valid xs n = not $ null pairs
 section :: [Int] -> Int -> Int -> [Int]
 section xs i n = (take n . drop i) xs
 
-solve1 :: [Int] -> Preamble -> Maybe Int
-solve1 xs p = case (length ys) of
-    0 -> Nothing
-    _ -> Just $ head ys
+solve1 :: [Int] -> Preamble -> Int
+solve1 xs p = head ys
     where ys =
             [ x
             | (x, i) <- zip (drop p xs) [p..]
             , not $ valid (section xs (i - p) p) x
             ]
 
+helper :: [Int] -> Int -> [Int] -> Int
+helper xs n (y:ys) = if null ss
+    then helper xs n ys
+    else minimum s + maximum s
+        where s = head ss
+              ss =
+                [ sec
+                | (z, i) <- zip xs [0..(length xs)-y]
+                , let sec = section xs i y
+                , sum sec == n
+                ]
+
+solve2 :: [Int] -> Int -> Int
+solve2 xs n = helper xs n [2..length xs]
+
 main :: IO ()
 main = do
     { args <- getArgs
     ; xs <- parse $ head args
     ; let preamble = read $ args !! 1
-    ; putStrLn $ "Part 1: " ++ show (solve1 xs preamble)
+    ; let n = solve1 xs preamble
+    ; putStrLn $ "Part 1: " ++ show n
+    ; putStrLn $ "Part 2: " ++ show (solve2 xs n)
     }
