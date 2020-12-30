006caa1b457312f136796d0c15fe3106d4f7ce90
daniope <daniela.iope@hotmail.com>
2020-12-29 22:52:36 -0300

    feat: impls solution for part 1

--- day10/Main.hs
+++ day10/Main.hs
@@ -1,14 +1,23 @@
 module Main where
 
+import Data.List
 import System.Environment
 
 parse :: FilePath -> IO [Int]
 parse = fmap rd . (fmap lines . readFile)
     where rd = map read
 
+intervals :: [Int] -> [Int]
+intervals (x:[]) = []
+intervals (x:xs) = head xs - x : intervals xs
+
+solve1 :: [Int] -> Int
+solve1 xs = (length $ filter (==1) ys) * ((length $ filter (==3) ys) + 1)
+    where ys = intervals $ sort $ 0:xs
+
 main :: IO ()
 main = do
     { args <- getArgs
-    ; js <- parse $ head args
-    ; putStrLn $ "Part 1: " ++ show js
+    ; xs <- parse $ head args
+    ; putStrLn $ "Part 1: " ++ show (solve1 xs)
     }
