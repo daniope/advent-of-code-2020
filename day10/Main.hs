541f9f2cfae1a25fe4ac52bca6b7719292a1f116
daniope <daniela.iope@hotmail.com>
2020-12-29 22:13:46 -0300

    feat: impls parsing of input files

--- day10/Main.hs
+++ day10/Main.hs
@@ -2,5 +2,13 @@ module Main where
 
 import System.Environment
 
+parse :: FilePath -> IO [Int]
+parse = fmap rd . (fmap lines . readFile)
+    where rd = map read
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    { args <- getArgs
+    ; js <- parse $ head args
+    ; putStrLn $ "Part 1: " ++ show js
+    }
