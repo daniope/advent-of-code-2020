1efc8e84131b623b130c7b5c3df8624cb01c2d2e
daniope <daniela.iope@hotmail.com>
2021-01-03 23:19:00 -0300

    feat: impls parsing of isntructions

--- day12/Main.hs
+++ day12/Main.hs
@@ -1,4 +1,65 @@
 module Main where
 
+import Prelude -- hiding (Right, Left)
+import Text.Parsec
+import Text.Parsec.String
+import System.Environment
+
+data Action
+    = N
+    | E
+    | S
+    | W
+    | F
+    | R
+    | L
+    deriving (Eq, Show)
+
+type Instruction = (Action, Int)
+
+data Direction
+    = North
+    | South
+    | East
+    | West
+    deriving (Show)
+
+type Position =((Direction, Int), (Direction, Int))
+
+data Ship = Ship
+    { position :: Position
+    , path     :: [Position]
+    } deriving (Show)
+
+toAction :: Char -> Action
+toAction c = case c of
+    'N' -> N
+    'E' -> E
+    'S' -> S
+    'W' -> W
+    'F' -> F
+    'R' -> R
+    'L' -> L
+    otherwise -> error "Invalid direction"
+
+integer :: Parser Int
+integer = fmap read $ many1 digit
+
+instruction :: Parser Instruction
+instruction = do
+    { d <- letter
+    ; n <- integer
+    ; return (toAction d, n)
+    }
+
+instructions :: Parser [Instruction]
+instructions = endBy1 instruction endOfLine
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    { args <- getArgs
+    ; content <- parseFromFile instructions $ head args
+    ; case content of
+        Left err -> print err
+        Right cs -> putStrLn $ "Part 1: " ++ show cs
+    }
