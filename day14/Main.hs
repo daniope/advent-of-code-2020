6e78320e4aa2a8c003316a78fabdc7dac1c197d7
daniope <daniela.iope@hotmail.com>
2021-01-07 23:01:00 -0300

    fix: corrects parsing of bit mask

--- day14/Main.hs
+++ day14/Main.hs
@@ -1,10 +1,14 @@
 module Main where
 
+import Numeric (showHex, showIntAtBase)
+import Data.Char (intToDigit)
+import Data.Bits hiding (bit)
+import Data.Maybe
 import Text.Parsec
 import Text.Parsec.String
 import System.Environment
 
-type BitMask = String
+type BitMask = [(Int, Int)]
 type Value = (Int, Int)
 
 data Instruction = Instruction
@@ -17,11 +21,15 @@ type Program = [Instruction]
 integer :: Parser Int
 integer = fmap read $ many1 digit
 
+bit :: Parser (Maybe Char)
+bit = try (Just <$> digit) <|> (char 'X' >> return Nothing)
+
 bitMask :: Parser BitMask
 bitMask = do
     { _ <- string "mask = "
-    ; m <- many1 alphaNum
-    ; return m
+    ; bs <- many1 bit
+    ; let l = length bs
+    ; return [ (i, read [b]) | (i, Just b) <- zip [l-1,l-2..0] bs ]
     }
 
 value :: Parser Value
@@ -44,10 +52,16 @@ instruction = do
 program :: Parser Program
 program = many1 instruction
 
+encode :: Value -> BitMask -> Value
+encode v [] = v
+encode (loc, x) ((i, b):bm) 
+    | b == 0 = encode (loc, clearBit x i) bm
+    | b == 1 = encode (loc, setBit x i) bm
+
 main :: IO ()
 main = do
     { args <- getArgs
-    ; result <- parseFromFile program $ head args
+    ; result <- parseFromFile instruction $ head args
     ; case result of
         Left err -> print err
         Right r -> do
